ea1803d79e1ff74f59403e375c68297b2e1f968d
Rohit Pandit <rohit0311@outlook.com>
2021-02-23 15:13:52 +0530

    Context for contacts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import useLocalStorage from '../hooks/useLocalStorage.js';
 import Login from './Login.js';
 import Dashboard from './Dashboard.js';
+import { ContactsProvider } from '../contexts/ContactsProvider';
 
 function App() {
 	const [id, setId] = useLocalStorage('id');
 
-	return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
+	const dashboard = (
+		<ContactsProvider>
+			<Dashboard id={id} />
+		</ContactsProvider>
+	);
+
+	return id ? dashboard : <Login onIdSubmit={setId} />;
 }
 
 export default App;
--- client/src/components/NewContactModal.js
+++ client/src/components/NewContactModal.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Fragment, useRef } from 'react';
+import { Modal, Form, Button } from 'react-bootstrap';
+import { useContacts } from '../contexts/ContactsProvider';
 
-const NewContactModal = () => {
-	return <div>New Contact Modal</div>;
+const NewContactModal = ({ closeModal }) => {
+	const idRef = useRef();
+	const nameRef = useRef();
+	const { createContact } = useContacts();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		createContact(idRef.current.value, nameRef.current.value);
+		closeModal();
+	};
+
+	return (
+		<Fragment>
+			<Modal.Header closeButton>Create Contact</Modal.Header>
+			<Modal.Body>
+				<Form onSubmit={handleSubmit}>
+					<Form.Group>
+						<Form.Label>Id </Form.Label>
+						<Form.Control type='text' ref={idRef} required />
+					</Form.Group>
+					<Form.Group>
+						<Form.Label>Name </Form.Label>
+						<Form.Control type='text' ref={nameRef} required />
+					</Form.Group>
+					<Button type='submit'>Create</Button>
+				</Form>
+			</Modal.Body>
+		</Fragment>
+	);
 };
 
 export default NewContactModal;
